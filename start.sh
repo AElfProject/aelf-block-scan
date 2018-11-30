b44d7e1a742fbd22a9ce84b50cca0357b8522eef
hzz780 <hzz780@qq.com>
2018-11-30 12:41:39 +0800

    start.sh add git pull, reisntall node_modules;

--- start.sh
+++ start.sh
@@ -1,10 +1,21 @@
 # 启动scan程序后，如果完成扫链，会调用initTpsAcquisition.js启动采集程序。
 
-if [ $1 == "dev" ]
+echo 'git pull';
+git pull
+
+if [ $2 == 'reinstall' ]
 then
-    pm2 start index.js --name aelf-block-scan
-    echo "pm2 start index.js --name aelf-block-scan";
+    echo 'rm -rf node_modules';
+    #rm -rf node_modules;
+    echo 'npm install';
+    #npm install;
+fi
+
+if [ $1 == 'dev' ]
+then
+    #pm2 start index.js --name aelf-block-scan
+    echo 'pm2 start index.js --name aelf-block-scan';
 else
-    NODE_ENV=production pm2 start index.js --name aelf-block-scan
-    echo "NODE_ENV=production pm2 start index.js --name aelf-block-scan";
+    #NODE_ENV=production pm2 start index.js --name aelf-block-scan
+    echo 'NODE_ENV=production pm2 start index.js --name aelf-block-scan';
 fi
