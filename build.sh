c1435e9888c8ce1aabc13e4f167a5a7d96c2664e
hzz780 <hzz780@qq.com>
2018-12-01 14:43:28 +0800

    fix shell for linux, update sdk version ->1.0.10

--- build.sh
+++ build.sh
@@ -1,26 +1,37 @@
+#!/bin/bash
+# https://www.leiphone.com/news/201703/i49ztcRDDymM7Id5.html // Bash编程细节
 # 启动scan程序后，如果完成扫链，会调用initTpsAcquisition.js启动采集程序。
 
-echo 'git pull';
+#当变量a为null或为空字符串时则var=b
+start_mode=${1:-'production'}
+node_modules_action=${2:-'default'}
+echo ${node_modules_action} ${start_mode}
+
+echo 'git pull'
 git pull
 
-if [ $2 == 'reinstall' ]
+if [ ${node_modules_action} = 'reinstall' ]
 then
-    echo 'rm -rf node_modules';
-    rm -rf node_modules;
-    echo 'npm install';
-    npm install;
+    echo 'rm -rf node_modules'
+    rm -rf node_modules
+    echo 'npm install'
+    npm install
 fi
 
-pm2 stop aelf-block-scan;
-pm2 stop aelf-block-scan-tps;
-pm2 delete aelf-block-scan
-pm2 delete aelf-block-scan-tps;
+app_names=('aelf-block-scan', 'aelf-block-scan-tps')
 
-if [ $1 == 'dev' ]
+for item in ${app_names[@]}
+do
+    echo 'pm2 stop & delete ' ${item};
+    pm2 stop ${item}
+    pm2 delete ${item}
+done
+
+if [ ${start_mode} = 'dev' ]
 then
     pm2 start index.js --name aelf-block-scan
-    echo 'pm2 start index.js --name aelf-block-scan';
+    echo 'pm2 start index.js --name aelf-block-scan'
 else
     NODE_ENV=production pm2 start index.js --name aelf-block-scan
-    echo 'NODE_ENV=production pm2 start index.js --name aelf-block-scan';
+    echo 'NODE_ENV=production pm2 start index.js --name aelf-block-scan'
 fi
